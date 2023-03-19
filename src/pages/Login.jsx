6516d3b6c99ce87cb71fcc98584ad194c98fc3a8
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-03-19 17:15:16 +0600

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,9 +7,12 @@ import {
   MDBInput,
   MDBValidation,
 } from "mdb-react-ui-kit";
-import { Link } from "react-router-dom";
 import React from "react";
 import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { login } from "../redux/features/authSlice";
 
 const initialState = {
   email: "",
@@ -20,8 +23,14 @@ const Login = () => {
   const [formValue, setFormValue] = useState(initialState);
   const { email, password } = formValue;
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (email && password) {
+      dispatch(login({ formValue, navigate, toast }));
+    }
   };
 
   const onInputChange = (e) => {
